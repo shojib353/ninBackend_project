d7f900f7c177ad34bf07f928d56ac13aac3504c1
shojib353 <shojib.shojib10@gmail.com>
2026-05-02 23:57:28 +0600

    fix: remove sqlite, staticfiles, add runtime.txt, fix settings for render

--- ninProject/settings.py
+++ ninProject/settings.py
@@ -206,6 +206,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"   # destination folder (IMPORTANT)
 # Media files (uploaded images)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
