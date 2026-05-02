49f7ce3c6c2c039aba807fa965a840206afe895b
MD Shahoriar Hossain Shojib <99172514+shojib353@users.noreply.github.com>
2026-05-03 00:28:08 +0600

    Setup Django for ASGI application with channels

--- ninProject/asgi.py
+++ ninProject/asgi.py
@@ -1,14 +1,16 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ninProject.settings")
+
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import websocket_urlpatterns
 from chat.middleware import JWTAuthMiddleware
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ninProject.settings")
-
 django_asgi_app = get_asgi_application()
-
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
